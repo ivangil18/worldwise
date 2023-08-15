c279def439784973f7eda5dac194c6182ae1ba5c
Ivan Gil <igil.dev@gmail.com>
2023-08-15 16:24:34 -0400

    map relocation per city selected implemented

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,31 +1,64 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useCities } from "../contexts/CitiesContext";
+
 import styles from "./Map.module.css";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 
 function Map() {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const navigate = useNavigate();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const [searchParams] = useSearchParams();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
 
-  console.log(lat, lng);
+  const { cities, currentCity } = useCities();
+
+  console.log(currentCity.cityName);
+
+  const mapLat = searchParams.get("lat");
+  const mapLng = searchParams.get("lng");
+
+  useEffect(
+    function () {
+      if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+    },
+    [mapLat, mapLng]
+  );
+
+  const navigate = useNavigate();
 
   return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <h1>Position</h1>
-      <p>{lat}</p>
-      <p>{lng}</p>
-      <button
-        onClick={() => {
-          setSearchParams({
-            lat: 23,
-            lng: 50,
-          });
-        }}
+    <div className={styles.mapContainer}>
+      <MapContainer
+        center={mapPosition}
+        zoom={6}
+        scrollWheelZoom={true}
+        className={styles.map}
       >
-        Update Param
-      </button>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji} </span>{" "}
+              <span>
+                {city.cityName}, {city.country}
+              </span>
+            </Popup>
+          </Marker>
+        ))}
+        <RelocateMap position={mapPosition} />
+      </MapContainer>
     </div>
   );
+}
+function RelocateMap({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
 }
 
 export default Map;
